becb4b6bca4a14b64a3e7774a76121905ed2d605
zana-abdollahpour <zana.ap4@gmail.com>
2026-02-11 02:31:57 +0330

    useDebounce

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export function useDebounce<TValue>(value: TValue, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
