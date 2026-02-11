62297177e34d4541b02648ee34187a87a4a62649
zana-abdollahpour <zana.ap4@gmail.com>
2026-02-11 20:16:22 +0330

    useMouse

---
+++ src/hooks/useMouse.ts
@@ -0,0 +1,58 @@
+import { useLayoutEffect, useRef, useState } from "react";
+
+interface MouseState {
+  x: number;
+  y: number;
+  elementX: number;
+  elementY: number;
+  elementPositionX: number;
+  elementPositionY: number;
+}
+
+export function useMouse() {
+  const [state, setState] = useState<MouseState>({
+    x: 0,
+    y: 0,
+    elementX: 0,
+    elementY: 0,
+    elementPositionX: 0,
+    elementPositionY: 0,
+  });
+
+  const elementRef = useRef<HTMLElement | null>(null);
+
+  useLayoutEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      const newState: Partial<MouseState> = {
+        x: event.pageX,
+        y: event.pageY,
+      };
+
+      if (
+        elementRef.current &&
+        elementRef.current.nodeType === Node.ELEMENT_NODE
+      ) {
+        const { left, top } = elementRef.current.getBoundingClientRect();
+        const elementPositionX = left + window.scrollX;
+        const elementPositionY = top + window.scrollY;
+        const elementX = event.pageX - elementPositionX;
+        const elementY = event.pageY - elementPositionY;
+
+        newState.elementX = elementX;
+        newState.elementY = elementY;
+        newState.elementPositionX = elementPositionX;
+        newState.elementPositionY = elementPositionY;
+      }
+
+      setState((cur) => ({ ...cur, ...newState }));
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return [state, elementRef];
+}
